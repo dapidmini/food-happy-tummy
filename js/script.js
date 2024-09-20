af2ddf95cdabd127d230371d7158585e9f88afdd
dapidmini <david.santana.1112@gmail.com>
2024-09-21 01:27:13 +0700

    perbaiki section menu - coba pake swipeJS

---
+++ js/script.js
@@ -0,0 +1,18 @@
+var swiper = new Swiper(".menu-list-container", {
+  effect: "coverflow",
+  initialSlide: 1,
+  loop: true,
+  grabCursor: true,
+  centeredSlides: true,
+  slidesPerView: "auto",
+  coverflowEffect: {
+    rotate: 50,
+    stretch: 0,
+    depth: 100,
+    modifier: 1,
+    slideShadows: true,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+  },
+});
