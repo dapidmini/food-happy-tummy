b70cf56d141dc72119c602cffef87f7b35c25f82
dapidmini <david.santana.1112@gmail.com>
2024-09-28 02:52:04 +0700

    lanjutkan bag.menu

--- js/script.js
+++ js/script.js
@@ -1,6 +1,5 @@
 var swiper = new Swiper(".menu-list-container", {
   effect: "coverflow",
-  initialSlide: 0,
   loop: true,
   grabCursor: true,
   centeredSlides: true,
@@ -17,44 +16,62 @@ var swiper = new Swiper(".menu-list-container", {
   },
 });
 
-document.addEventListener('click', function(e) {
-  e.preventDefault();
-  // reset semua elemen .swiper-slide
-  // hilangkan atribut data-bs-toggle dari semua button modalnya
-  // spy popup modal hanya terjadi kalo klik di button swiper yg sedang aktif
-  const allSwiperButtons = document.querySelectorAll('.menu .btn-view-gallery');
-  allSwiperButtons.forEach(elem => {
-    elem.removeAttribute('data-bs-toggle');
-  });
-
-  const clickedEl = e.target;
-  if (clickedEl.closest('.swiper-slide-active')) {
-    const modalSelector = clickedEl.getAttribute('data-bs-target');
-    const modalElem = document.querySelector(modalSelector);
-    modalElem.setAttribute('data-bs-toggle', 'modal');
-  }
-  // const clickedEl = e.target;
-  // const parentEl = clickedEl.parentElement;
-
-  // console.log('debug', clickedEl, parentEl, clickedEl.closest('.swiper-slide-active').length);
-});
 
 // document.addEventListener('DOMContentLoaded', function(e) {
-//   cekActiveSliderBtn(clickedEl);
-// })
+//   // ketika klik salah satu tombol utk lihat gallery menu masing2 item
+//   const btnMenuGallery = document.querySelectorAll('.btn-view-gallery');
+//   btnMenuGallery.forEach(elem => {
+//     elem.addEventListener('click', function(ee) {
+//       const wrapper = elem.closest('[data-menu-name]');
+//       const menuName = wrapper.getAttribute('data-menu-name');
+//       const menuClass = wrapper.getAttribute('data-menu-class');
+//       const modalElem = document.querySelector('.modal#modalMenu');
+//       const modalTitle = modalElem.querySelector('.modal-title');
+//       modalTitle.innerHTML = menuName;
+//       modalTitle.classList.add('text-capitalize');
 
-const swiperSlides = document.querySelectorAll('.swiper-slide');
-swiperSlides.forEach(elem => {
-  elem.addEventListener('click', function(e) {
-    cekActiveSliderBtn(this);
-  });
-});
+//       var swiperMenuModal = new Swiper(".swiper-menu-modal", {
+//         effect: "flip",
+//         grabCursor: true,
+//         loop: true,
+//         pagination: {
+//           el: ".swiper-pagination",
+//         },
+//         navigation: {
+//           nextEl: ".swiper-button-next",
+//           prevEl: ".swiper-button-prev",
+//         },
+//       });
 
-function cekActiveSliderBtn(elem) {
-  if (elem.classList.contains('swiper-slide-active')) {
-    const btnGallery = elem.querySelector('.btn-view-gallery');
-    btnGallery.addEventListener('click', function(ee) {
-      alert('clicked on active button');
-    });
-  }
-}
+//       let useSlides = [];
+//       const galleryClass = 'items-gallery-' + menuClass;
+//       // console.log('menu', menuName+';'+menuClass+';'+galleryClass);
+//       const slides = document.querySelectorAll('.menu .modal .swiper-slide');
+//       slides.forEach((slide, index) => {
+//         // console.log('slide', index, slide.classList.contains(galleryClass));
+//         if (slide.classList.contains(galleryClass)) {
+//           useSlides.push(slide);
+//           // swiperMenuModal.appendSlide(slide);
+//         // } else {
+//         //   swiperMenuModal.removeSlide(index);
+//         }
+//       });
+
+//       swiperMenuModal.removeAllSlides();
+//       swiperMenuModal.appendSlide(useSlides);
+
+//       // console.log('remove indexes', removeIndex);
+
+//       // swiperMenuModal.removeSlide(removeIndex);
+//       // const swiperWrappers = document.querySelectorAll('.modal .swiper-wrapper');
+//       // swiperWrappers.forEach(wrapper => {
+//       //   if (wrapper.id == galleryId) {
+//       //     wrapper.classList.remove('d-none');
+//       //   } else {
+//       //     wrapper.classList.add('d-none');
+//       //   }
+//       //   // wrapper.classList.add('d-none');
+//       // });
+//     });
+//   });
+// });
